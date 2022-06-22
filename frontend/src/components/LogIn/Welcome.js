69f586f6236a286c2d74751ae0e0ebdcce71a048
Neaa99 <linnea@kringelkroken.se>
2022-06-22 13:57:31 +0200

    clean code

--- frontend/src/components/LogIn/Welcome.js
+++ frontend/src/components/LogIn/Welcome.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
@@ -12,7 +12,7 @@ const Welcome = () => {
             <div className="title-container">
                 <h3 className="main-title"></h3>
             </div>
-            
+
             <h1 className="welcome">{user.superhero ? `Welcome ${user.superhero}` : `Welcome ${user.fullName}` ? !user.superhero : `Welcome`}</h1>
             <div className="welcome-container">
                 <Link to="/authorized/profile" className='welcome-link'>
